80676620e1d2a252d266e18ec14efd0a24b3a343
MertFGunes <mertfaruk.ie@gmail.com>
2024-10-20 19:52:10 +0100

    20/10/2024
    a little bit of css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,16 @@
-const Nav = () => {
+import outtonight from '../images/outtonight.png'
+
+const Nav = ({minimal, authToken}) => {
+
+
     return (
-        <div>Nav home</div>
+        <nav>
+            <div className="logo-container">
+                <img className="logo" src={minimal ? outtonight : minimal} />
+            </div>
+
+            {!authToken && minimal && <button className= "nav-button" >Log in</button>}
+        </nav>
     )
 }
 export default Nav
