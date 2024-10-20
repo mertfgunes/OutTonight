67f82cb6e13420f5de1cf96bbb1833bfd13f0ef6
MertFGunes <mertfaruk.ie@gmail.com>
2024-10-20 17:42:40 +0100

    css has been added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import OnBoarding from './pages/OnBoarding'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,22 @@
+import Nav from '../components/Nav'
+
 const Home = () => {
+
+    const authToken = true
+
+    const handleClick = () => {
+        console.log('clicked')
+    }
+
     return (
-        <div>Home</div>
+        <>
+        <div className = "home">
+            <h1> Swipe Right</h1>
+            <button className= "primary-button" onClick={handleClick}>
+                {authToken ? 'Sign out' : 'Create an account'}
+            </button>
+        </div>
+        </>
     )
 }
 export default Home
