05e6bea56b18136a5e9b0b91d811c6e414278b1a
MertFGunes <mertfaruk.ie@gmail.com>
2024-10-20 14:26:14 +0100

    idk man

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import OnBoarding from './pages/OnBoarding'
-import{ BrowserRouter, Routes, Route } from 'react-route-dom'
+import { BrowserRouter, Routes, Route } from 'react-route-dom'
+
 const App = () => {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element ={<Home/>}/>
-          <Route path="/" element ={<Dashboard/>}/>
-          <Route path="/" element ={<OnBoarding/>}/>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route  path="/" element={<Home/>} />
+        <Route  path="/Dashboard" element={<Dashboard/>} />
+        <Route  path="/OnBoarding" element={<OnBoarding/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
