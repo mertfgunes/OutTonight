f8854d3f7ee3f5dabd1b60246ba07b7c1f283b7b
MertFGunes <mertfaruk.ie@gmail.com>
2024-10-20 13:40:48 +0100

    Router has been set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import OnBoarding from './pages/OnBoarding'
+import{ BrowserRouter, Routes, Route } from 'react-route-dom'
 const App = () => {
   return (
-    <div>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element ={<Home/>}/>
+          <Route path="/" element ={<Dashboard/>}/>
+          <Route path="/" element ={<OnBoarding/>}/>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
